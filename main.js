407acf37c0bcd6855b01bed44a5176d1ea756bf8
anosci <ano.sci4@gmail.com>
2015-09-22 22:26:38 -0500

    implemented era highlighting

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ eraray = [];
 function ret0if(thisIsUndefined){
 	return typeof thisIsUndefined==="undefined"?0:thisIsUndefined;
 }
+Date.prototype.getWeek = function() {
+var onejan = new Date(this.getFullYear(),0,1);
+return Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
+}
 function createeve(e,n,t,label,desc,tags){
 	everay.push(new eve())
-	everay[everay.length-1].setDate(e,n,t)
+	everay[everay.length-1].setDate(e,n-1,t)
 	everay[everay.length-1].label = label
 	if(ret0if(desc)!=0){
 		everay[everay.length-1].desc = desc
@@ -14,15 +18,18 @@ function createeve(e,n,t,label,desc,tags){
 		everay[everay.length-1].setTags(tags)
 	}
 }
-function createera(e,n,t,label,desc,tags){
+function createera(fromeve,toeve,label,color,desc,tags){
 	eraray.push(new era())
-	eraray[eraray.length-1].setDate(e,n,t)
 	eraray[eraray.length-1].label = label
+	eraray[eraray.length-1].range = [fromeve,toeve]
+	if(ret0if(color)!=0){
+		eraray[eraray.length-1].color = color
+	}
 	if(ret0if(desc)!=0){
-		everay[everay.length-1].desc = desc
+		eraray[eraray.length-1].desc = desc
 	}
 	if(ret0if(tags)!=0){
-		everay[everay.length-1].setTags(tags)
+		eraray[eraray.length-1].setTags(tags)
 	}
 }
 function getevefromlabel(label,year,month,day){
@@ -69,6 +76,8 @@ function era(){
 	this.desc = "";
 	this.tags = [];
 	this.range = [];
+	this.drawme = false;	//future implimentation: list eras, select which ones to draw, and toggle this flag. 
+	this.color = "006";
 	this.setRange = function(beg,end){
 		this.range = [beg,end]
 	}
@@ -88,8 +97,29 @@ function drawtable(){
 	cout =""
 	for(y=0;y<90;y++){
 		cout+='<div class="leftside">year '+y+'</div>'
-		for(x=0;x<65;x++){
-			cout+='<div class="bg0">'
+		for(x=0;x<52;x++){
+			skipme = false;
+			thisblock = new Date(getevefromlabel("%begin").date.getFullYear()+y,getevefromlabel("%begin").date.getMonth(),getevefromlabel("%begin").date.getDate());
+			thisblock = new Date(Date.parse(thisblock) + 1000*60*60*24*7*x)
+			for(z=0;z<eraray.length;z++){
+				if(eraray[z].range[0].date<=thisblock){
+					//years line up. now see if there's an offset (i.e., begin at jan1 or begin at birthday)
+					if(eraray[z].range[1].date>=thisblock){
+						if(eraray[z].label != "uneventful life"){
+							alert((eraray[z].range[0].label) )
+						}
+						//color time
+						cout+='<div class="bg0" style="background-color:#'
+						cout+=eraray[z].color;
+						cout+=';">'
+						skipme=true;
+						break;
+					}
+				}
+			}
+			if(!skipme){
+				cout+='<div class="bg0">'
+			}
 			cout+='<div class="w">'
 			cout+='</div>'
 			cout+='</div>'
@@ -98,4 +128,5 @@ function drawtable(){
 	}
 	gid("field").innerHTML=cout;
 }
-drawtable()
+createeve(1990,1,1,"%begin","","meta")
+
